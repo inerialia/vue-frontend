e290b683a3a35f94634c15fe5f23aef98d3befd4
Inerialia <annashar@gmail.com>
2021-04-23 20:28:53 +0200

    a

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hr />
+    <UploadComponent />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import UploadComponent from './components/UploadComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    UploadComponent
   }
 }
 </script>
